b173bf557b3d2c1f0e1ab680820d5e534728bf9d
sarojladdha <sarojladdha@hotmail.com>
2024-08-28 12:14:03 +0530

    added Comment in Entity Model

--- SahadevBusinessEntity/DTO/Model/DossierRecep.cs
+++ SahadevBusinessEntity/DTO/Model/DossierRecep.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*  --------------------------------------------------------------------------------------------*
+ *  Class Name      :-  DossierRecep                                                            *
+ *  --------------------------------------------------------------------------------------------*
+ *  Description     :-  Entity Model for the Dossier DossierRecep                               *
+ *  --------------------------------------------------------------------------------------------*
+ *  Created By      :- Saroj Laddha                                                             *
+ *  --------------------------------------------------------------------------------------------*
+ *  Created Date    :- 24-Aug-2024                                                              *
+ *  --------------------------------------------------------------------------------------------*
+ *  revised By      :-                                                                          *
+ *  revised Details :-                                                                          *
+ *  revised By      :-                                                                          *
+ *  revised Details :-                                                                          *
+ //**********************************************************************************************/
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,5 +26,6 @@ namespace SahadevBusinessEntity.DTO.Model
         public int DossierRecepID { get; set; }
         public int DossierDefID { get; set; }
         public int UserID { get; set; }
+
     }
 }
